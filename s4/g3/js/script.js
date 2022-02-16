c489732a094903ec037a974a37312cd2105281e6
HamzaMajeed97 <hamzamajeed784@gmail.com>
2022-02-16 22:34:12 +0100

    funzioni

---
+++ s4/g3/js/script.js
@@ -0,0 +1,54 @@
+
+
+/*var anni = 17;
+
+var ingresso = (anni>=18) ? "ingresso consentito" : "ingresso vietato";
+
+alert(ingresso); */
+
+
+
+
+
+
+
+ 
+ // funzione con freccia 
+ 
+ var saluto = () => {return "Ciao a tutti"}
+
+console.log(saluto());
+
+
+// funzione con closures 
+
+
+function operazioni (){
+    var numero = 0;
+
+    function somma () {
+    console.log(a+b);
+}
+return somma 
+
+}
+
+var numero1= operazioni()
+
+console.log(numero1);
+
+
+
+
+
+
+
+
+// operazioni 
+
+console.log("Mario"=== "Mario"); 
+console.log(10<9);
+console.log(5>=5);
+console.log(2 !== 2);
+console.log(3!=6);
+
