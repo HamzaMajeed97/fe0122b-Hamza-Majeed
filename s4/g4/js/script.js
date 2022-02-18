58078c5c4af5008c0fdaff35b0bf22b3ad951506
HamzaMajeed97 <hamzamajeed784@gmail.com>
2022-02-18 10:25:17 +0100

    progetto

---
+++ s4/g4/js/script.js
@@ -0,0 +1,21 @@
+
+var nome = "Pippo";
+console.log(nome)
+
+
+
+
+
+
+var persone =["Marco","Mario","Francesco","Giovanni","Emanuele"]
+
+
+
+ var aggiuntapersone = persone.push("Giovanni");
+console.log(aggiuntapersone);
+
+
+
+
+console.log(persone.length);
+
